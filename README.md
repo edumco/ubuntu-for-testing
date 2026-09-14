b861a9e4e2456524720aa0ae67897886a1529da7
José Eduardo Montenegro <edu.pombos@hotmail.com>
2026-09-14 09:13:40 -0300

    Adds main script and updates documentation

--- README.md
+++ README.md
@@ -1,6 +1,61 @@
-# Ubuntu for testing 
-Ubuntu configurations for testers and devs
+# Ubuntu for Testing
 
-![ladybug](assets/ladybug-dooder.png)
+> Scripts to easily install, manage and update tools on Ubuntu
 
-Icon By [Dooder](https://icon-icons.com/authors/967-dooder)
+Ubuntu does not come by default with support for several Linux app types and utilities. They are scattered across repositories and have several installation processes. 
+
+This repository creates an unified process of update and helps installations through a collection of scripts focused on giving you the best tools for daily use that you can carry with you in all your projects.
+
+## Setup
+
+Clone this project and execute the 'ubuntu-setup.sh' script:
+
+```bash
+    git clone https://github.com/edumco/ubuntu-for-testing.git
+    cd ubuntu-for-testing && chmod +x ubuntu_setup.sh
+    ./ubuntu_setup.sh
+    cd tools && chmod +x $(ls) && cd ..
+    cd environments && chmod +x $(ls) && cd ..
+```
+
+After the installations you'll be able to:
+
+    - Install Flatpak and AppImage apps 
+
+    - Install apps directly from GitHub repositories
+
+    - Update all your apps with a single command:
+
+    ```bash
+        update
+    ```
+
+## Install other tools
+	
+You can install environments and  individually or run all the scripts inside a folder:
+
+```bash
+    cat $(ls) | sh 
+```
+
+## How it works
+	
+The main script installs some app managers (flatpak, AppManage and DRA), creates a local script to keep all update commands together  ('update.sh') and finally creates an alias to this script.
+
+Every time you enter 'update' on the terminal, all the update and cleanup commands are executed.
+ 
+To keep consistent behavior, every new environment script adds its own update and cleanup commands to the end of update file.
+
+```bash
+echo "uv self update && 	uv python upgrade && cache prune" >> ~/.update.sh 
+```
+
+References:
+	https://yarygintech.com/articles/flatpak-vs-snap-vs-appimage/
+	https://flathub.org/
+	https://www.appimagehub.com/
+	https://github.com/kem-a/AppManager
+
+
+
+
---
+++ ubuntu_setup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Updates all apps
+
+
+	# Deb packages
+	sudo apt-get update && sudo apt-get upgrade --yes
+	sudo apt-get autoremove --purge --yes # removes unused dependencies
+	sudo apt-get autoclean --yes # removes downloaded files
+	
+	# Snap packages
+	sudo snap set system refresh.retain=2 # reduce unused dependencies
+	sudo snap refresh
+	 
+# Essential Packages
+
+	sudo apt-get install --yes $(check-language-support) # Language Support
+	sudo apt-get install --yes ubuntu-drivers-common # Third-party drivers
+	sudo apt-get install --yes ubuntu-restricted-extras # Media codecs and fonts
+	sudo apt-get install --yes gstreamer1.0-plugins-bad # Extra media codecs
+	sudo apt-get install --yes curl wget # download tools used in scripts 
+	
+# Flatpak support
+
+	sudo apt-get install --yes flatpak
+	flatpak remote-add --user --if-not-exists \
+	flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# DRA (Download Release Assets from GitHub)
+
+	# Helps installation directly from GitHub 
+	curl --proto '=https' --tlsv1.2 -sSf \
+	https://raw.githubusercontent.com/devmatteini/dra/refs/heads/main/install.sh \
+	| bash -s -- --to $HOME/.local/bin
+
+# AppImage apps support
+	
+	# Installs support for old AppImage apps 
+	sudo apt-get --yes install libfuse2t64
+	
+	# Install AppManager (install and uninstall AppImages)
+	dra download https://github.com/kem-a/AppManager \
+		--select AppManager-{tag}-anylinux-x86_64.AppImage \
+		--output $HOME/AppManager && \
+		sudo chmod +x $HOME/AppManager &&
+		$HOME/./AppManager install $HOME/AppManager
+
+# Alias 'update'
+	
+	# Creates a script file on the HOME folder 
+	touch $HOME/.update.sh
+	sudo chmod +x $HOME/.update.sh
+
+	# Adds all update commands
+	
+	# Debs
+	echo "sudo apt-get update && sudo apt-get upgrade --yes" >> $HOME/.update.sh
+	echo "sudo apt-get autoremove --purge --yes" >> $HOME/.update.sh
+	echo "sudo apt-get autoclean --yes" >> $HOME/.update.sh
+	
+	# Snaps
+	echo "sudo snap refresh" >> $HOME/.update.sh
+	
+	# Flatpak
+	echo "flatpak update" >> $HOME/.update.sh
+	echo "flatpak uninstall --unused" >> $HOME/.update.sh
+	
+	# AppImage
+	echo "$HOME/./Applications/AppManager --update-all" >> $HOME/.update.sh
+	
+	# Make sure .bash_aliases exists
+	touch $HOME/.bash_aliases
+	
+	# Adds 'update' alias
+	echo "alias update='$HOME/./.update.sh'" >> $HOME/.bash_aliases
+	
+	# Reloads the bash session
+	source $HOME/.bashrc
+	
+	# Test the alias
+	update 
